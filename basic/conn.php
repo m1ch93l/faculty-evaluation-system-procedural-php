7cb145163e5170ab88dab05947f0175f8c052de7
m1ch93l <91449532+m1ch93l@users.noreply.github.com>
2021-10-02 16:23:05 +0800

    Delete basic directory

--- basic/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost","root","","basic_command");
-
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-?>
--- basic/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$id=$_GET['id'];
-	include('db_conn.php');
-	mysqli_query($conn,"delete from `student` where id='$id'");
-	header('location:student.php');
-?>
