80f4720ea08fe2ee8c722e3e2a38cfb82bfd49a9
kulotsystems <kulotsystems@gmail.com>
2021-10-24 19:51:14 +0800

    Added meta (viewport) tag.

--- navbar.php
+++ navbar.php
@@ -4,7 +4,9 @@
 
 <!-- kinuha ko sa font awesome free -->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-    
+
+<!-- para mag-adopt yung pixels sa mobile -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 </head>
 
 <body>
