23d5079681eaf7143941be98bee3f227165f86fe
m1ch93l <91449532+m1ch93l@users.noreply.github.com>
2022-01-19 13:28:06 +0800

    Update viewedituser.php

--- viewedituser.php
+++ viewedituser.php
@@ -1,5 +1,7 @@
-<?php
-    include'navbar.php';
+<?php include'includes/header.php'; ?>
+<body>
+    <?php
+    include'includes/admin-navbar.php';
     include 'koneksyon.php';
 
     $datauser = $_GET['user'];
@@ -7,24 +9,27 @@
     $qryview = "SELECT * FROM users WHERE username='$datauser' ";
     $result = mysqli_query($koneksyon, $qryview);
 
-    echo "<div class='container py-5'>";
+    echo "<main class='mt-5 pt-3'>";
+    echo "<div class='container-fluid'>";
     echo "<H2>USERS INFORMATION</H2>";
 
     if(mysqli_num_rows($result) > 0) {
         echo "<form action='#update.php' method='post'>";
         $row = mysqli_fetch_assoc($result);
 
-        echo "Username: <input type='text' name='username' value='".$row["username"]."'><br>";
-        echo "name:<input type='text' name='name' value='".$row["name"]."'><br>";
-        echo "<input type='submit' value='UPDATE'>";
+        echo "Username: <input class='form-control' type='text' name='username' value='".$row["username"]."'><br>";
+        echo "Name: <input class='form-control' type='text' name='name' value='".$row["usertype"]."'><br>";
+        echo "<input class='form-control btn-primary' type='submit' value='UPDATE'>";
         echo "</form>";
 
         echo "<form action='user_list.php'>";
-        echo "<button type='submit'>Cancel</button>";
+        echo "<button class='form-control btn-danger' type='submit'>Cancel</button>";
         echo "</form>";
         echo "</div>";
+        echo "</main>";
     }
     else {
       echo "No records found";
     }
 ?>
+</body>
